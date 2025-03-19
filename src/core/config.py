1da206d178bd7beb853a7d01b88cd5120d5f195c
N. L. <nino@jarvislab.io>
2025-03-19 22:34:50 +0100

    fix(creds): rrewrite secret name

--- src/core/config.py
+++ src/core/config.py
@@ -61,7 +61,7 @@ class AppSettings(BaseSettings):
             "auth_uri": "https://accounts.google.com/o/oauth2/auth",
             "token_uri": "https://oauth2.googleapis.com/token",
             "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
-            "client_x509_cert_url": environ["CLIENTx509"],
+            "client_x509_cert_url": environ["CLIENTX509"],
             "universe_domain": "googleapis.com"
         }
 
